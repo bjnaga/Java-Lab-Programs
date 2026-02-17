419fbf01b8ca95331f4f3247be24a8532b177bd1
Naga Ganesh B J <bjnaga@gmail.com>
2026-02-17 11:02:04 +0530

    Create inner-outer-class2.java

---
+++ theory/unit 1/inner-outer-class2.java
@@ -0,0 +1,16 @@
+//Private Inner Class
+class OuterClass {
+  int x = 10;
+
+  private class InnerClass {
+    int y = 5;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    OuterClass myOuter = new OuterClass();
+    OuterClass.InnerClass myInner = myOuter.new InnerClass(); // error
+    System.out.println(myInner.y + myOuter.x);
+  }
+}
