61b180b6efa64b984dd45e60391734a890c3f476
Naga Ganesh B J <bjnaga@gmail.com>
2026-02-24 10:11:17 +0530

    Add interface and class implementations for Graduates

---
+++ theory/unit 3/interfaceWIthExtends.java
@@ -0,0 +1,26 @@
+package com.interfaceEx.example;
+
+interface Graduates{  
+	void intelligence() ;
+	int x=10;
+}
+interface ChildInterface extends Graduates{
+	void intelligence2();
+}
+class Kee implements Graduates {
+	public void intelligence() {
+		
+	}
+}
+class Mahi implements ChildInterface {
+	
+	public void intelligence2() {
+		
+	}
+	public void intelligence() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
