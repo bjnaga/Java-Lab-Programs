fb6465649e981befcda2db9d62f9e27312fc8047
Naga Ganesh B J <bjnaga@gmail.com>
2026-02-24 10:00:11 +0530

    Add nested interface example in nestedInterface.java

---
+++ theory/unit 3/nestedInterface.java
@@ -0,0 +1,14 @@
+package com.interfaceEx.example;
+
+
+
+
+abstract class Mahi {
+	private interface Graduates{ // nested interface 
+		void intelligence() ;
+		int x=10;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
