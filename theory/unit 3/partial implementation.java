68ec60f0b85e916438b4164d1722a43c2e023774
Naga Ganesh B J <bjnaga@gmail.com>
2026-02-24 09:58:51 +0530

    Create partial implementation.java

---
+++ theory/unit 3/partial implementation.java
@@ -0,0 +1,13 @@
+package com.interfaceEx.example;
+
+interface Graduates{
+	void intelligence() ;
+	int x=10;
+}
+
+
+abstract class Mahi implements Graduates{
+	public static void main(String[] args) {
+		
+	}
+}
